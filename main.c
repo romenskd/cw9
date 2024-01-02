7c8eacb758d69c53987bd302546e97dbec999716
romenskd <romenskyid@gmail.com>
2024-01-02 01:20:58 +0100

    nie dorobiono 2 5 ukladow oraz macierz osobliwa

---
+++ main.c
@@ -0,0 +1,42 @@
+#include "gauss.h"
+#include "backsubst.h"
+#include "mat_io.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    int res;
+    Matrix *A = readSquareMatrixFromFile(argc > 1 ? argv[1] : "A.txt");
+    Matrix *b = readVectorFromFile(argc > 2 ? argv[2] : "b.txt");
+    Matrix *x;
+
+    if (A == NULL || b == NULL) {
+        fprintf(stderr, "Błąd! Nie udało się wczytać macierzy A lub wektora b.\n");
+        return -1;
+    }
+
+    printf("Macierz A:\n");
+    printToScreen(A);
+
+    printf("Wektor b:\n");
+    printToScreenB(b);
+
+        res = eliminate(A, b);
+        x = createMatrix(A->c, 1);
+
+        if (x != NULL) {
+            res = backsubst(x, A, b);
+
+            printf("Wektor wynikowy x:\n");
+            printToScreen(x);
+
+            freeMatrix(x);
+        } else {
+            fprintf(stderr, "Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
+        }
+
+    freeMatrix(A);
+    freeMatrix(b);
+
+    return 0;
+}
