f6e6cac957d3b5d64a60c795188e691051e773c7
ptrWilkoo <154998368+ptrWilkoo@users.noreply.github.com>
2024-01-02 23:35:52 +0100

    Update main.c
    Dodanie komentarzy do main

--- main.c
+++ main.c
@@ -6,11 +6,11 @@
 
 int main(int argc, char **argv) {
     int res;
-    Matrix *A = readFromFile(argc > 1 ? argv[1] : "A.txt");
-    Matrix *b = readFromFile(argc > 2 ? argv[2] : "b.txt");
+    Matrix *A = readFromFile(argc > 1 ? argv[1] : "A.txt");   //czytamy plik jako 1 argument - macierz 
+    Matrix *b = readFromFile(argc > 2 ? argv[2] : "b.txt");   //czyamy plik jako 2 argument - wektor 
     Matrix *x;
 
-    if (A == NULL || b == NULL) {
+    if (A == NULL || b == NULL) {   //ubezpieczenie 
         fprintf(stderr, "Błąd! Nie udało się wczytać macierzy A lub wektora b.\n");
         return -1;
     }
@@ -19,21 +19,33 @@ int main(int argc, char **argv) {
     printToScreen(A);
 
     printf("Wektor b:\n");
-    printToScreenB(b);
+    printToScreen(b);
+    
+    
+    double determinant = det(A);
 
-        res = eliminate(A, b);
-        x = createMatrix(A->c, 1);
+    //w przypadku gdy macierz osobliwa 
+    if (determinant == 0.0) {     
+        fprintf(stderr, "Błąd! Macierz jest osobliwa. Nie można zastosować eliminacji Gaussa.\n");
+        freeMatrix(A);
+        freeMatrix(b);
+        return -1;
+    }
+     
+    
+    res = eliminate(A, b);    //liczymy wektor wynikowy 
+    x = createMatrix(A->c, 1);  //tworzymy wektor wynikowy 
+ 
+    if (x != NULL) {     //ubezpieczenie 
+        
+        res = backsubst(x, A, b);
+        printf("Wektor wynikowy x:\n");
+        printToScreen(x);
 
-        if (x != NULL) {
-            res = backsubst(x, A, b);
-
-            printf("Wektor wynikowy x:\n");
-            printToScreenB(x);
-
-            freeMatrix(x);
-        } else {
-            fprintf(stderr, "Błąd! Nie mogłem utworzyć wektora wynikowego x.\n");
-        }
+        freeMatrix(x);
+    } else {
+        fprintf(stderr, "Błąd! Nie udalo sie utworzyć wektora wynikowego x.\n");
+    }
 
     freeMatrix(A);
     freeMatrix(b);
